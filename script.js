44d2d092b56a104cf306a7df148ceccb786ddc8d
Noushin Anika Khan <noushinanikakhan@gmail.com>
2025-08-28 14:27:18 +0600

    third commit

---
+++ script.js
@@ -0,0 +1,58 @@
+
+
+// call emergency
+
+document.getElementById("call-emergency").addEventListener("click",function(e)
+{e.preventDefault()
+    alert ("Calling National Emergency Service 999") 
+    return;
+})
+
+document.getElementById("call-police").addEventListener("click",function(e)
+{e.preventDefault()
+    alert ("Calling Police 999") 
+    return;
+})
+
+document.getElementById("call-fire").addEventListener("click",function(e)
+{e.preventDefault()
+    alert ("Calling Fire Service 999") 
+    return;
+})
+
+document.getElementById("call-Ambulance").addEventListener("click",function(e)
+{e.preventDefault()
+    alert ("Calling Ambulance 1994-999999") 
+    return;
+})
+
+document.getElementById("call-WomenChild").addEventListener("click",function(e)
+{e.preventDefault()
+    alert ("Calling Women & Child Helpline 109") 
+    return;
+})
+
+document.getElementById("call-Anti-Corruption").addEventListener("click",function(e)
+{e.preventDefault()
+    alert ("Calling Anti-Corruption 106") 
+    return;
+})
+
+document.getElementById("call-Electricity").addEventListener("click",function(e)
+{e.preventDefault()
+    alert ("Calling Electricity 16216") 
+    return;
+})
+
+document.getElementById("call-Brac").addEventListener("click",function(e)
+{e.preventDefault()
+    alert ("Calling Brac 16445") 
+    return;
+})
+
+document.getElementById("call-Railway").addEventListener("click",function(e)
+{e.preventDefault()
+    alert ("Calling Bangladesh Railway 163") 
+    return;
+})
+
